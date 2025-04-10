5831de5510d5ff0d3208cccdfe66bd8582c50fc0
Ubuntu <azureuser@ToTakeImage.22sc0nnashuefepgk1224jjpgg.dxbx.internal.cloudapp.net>
2025-04-10 07:21:46 +0000

    Activate deploy workflow

--- chatbot.py
+++ chatbot.py
@@ -3,11 +3,11 @@ import uuid
 import requests
 
 # Backend URLs define
-LOAD_CHAT_URL = "http://127.0.0.1:5000/load_chat/"
+LOAD_CHAT_URL = "http://0.0.0.0:5000/load_chat/"
 SAVE_CHAT_URL = "http://127.0.0.1:5000/save_chat/"
 DELETE_CHAT_URL = "http://127.0.0.1:5000/delete_chat/"
 UPLOAD_PDF_URL = "http://127.0.0.1:5000/upload_pdf/"
-CHAT_URL = "http://127.0.0.1:5000/chat/"
+CHAT_URL = "http://0.0.0.0:5000/chat/"
 RAG_CHAT_URL = "http://127.0.0.1:5000/rag_chat/"
 
 # Initialize session state
